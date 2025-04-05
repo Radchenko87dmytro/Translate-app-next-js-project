8a295a754ebb0b25ce7a51534b6c6e2d186ec523
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2025-04-05 14:14:28 +0200

    feature: added audio quotes

--- src/components/VoiceRecorder.tsx
+++ src/components/VoiceRecorder.tsx
@@ -1,166 +1,3 @@
-// "use client";
-
-// // import { ReactMic } from "react-mic";
-// import { useEffect, useState } from "react";
-// import { create, StoreApi, UseBoundStore } from "zustand";
-// import * as voice from "@/utils/voice";
-// import dynamic from "next/dynamic";
-
-// // Dynamically import ReactMic (avoids SSR)
-// const ReactMic = dynamic(
-//   () => import("react-mic").then((mod) => mod.ReactMic),
-//   {
-//     ssr: false,
-//   }
-// );
-
-// export interface VoiceStore {
-//   voices: Voice[];
-//   addVoice: (newVoice: Blob) => void;
-//   toggleAcceptance: (id: number) => void;
-// }
-
-// export interface Voice {
-//   id: number;
-//   blob: Blob;
-//   createdAt: Date;
-//   isAccepted: boolean;
-// }
-
-// const useStore: UseBoundStore<StoreApi<VoiceStore>> = create<VoiceStore>(
-//   (set) => ({
-//     voices: [],
-//     addVoice: (newVoice: Blob) =>
-//       set((state) => ({
-//         voices: [
-//           ...state.voices,
-//           voice.init(state.voices.length + 1, newVoice),
-//         ],
-//       })),
-//     toggleAcceptance: (id: number) =>
-//       set((state) => ({
-//         voices: state.voices.map((v) =>
-//           v.id === id ? { ...v, isAccepted: !v.isAccepted } : v
-//         ),
-//       })),
-//   })
-// );
-
-// function VoiceCounter() {
-//   const voices = useStore((state) => state.voices);
-//   const toggleAcceptance = useStore((state) => state.toggleAcceptance);
-
-//   return (
-//     <div>
-//       <h1 className="text-center text-lg font-semibold mb-4">
-//         {voices.length} Voice{voices.length !== 1 ? "s" : ""} Recorded
-//       </h1>
-
-//       <div className="flex flex-col gap-4">
-//         {voices.map((v) => (
-//           <div
-//             key={v.id}
-//             className="flex flex-col md:flex-row items-center bg-white shadow p-4 rounded-lg"
-//           >
-//             <div className="flex-1 w-full">
-//               <h3 className="text-md font-semibold mb-2">Playback #{v.id}</h3>
-//               <audio
-//                 controls
-//                 src={URL.createObjectURL(v.blob)}
-//                 className="w-full"
-//               />
-//             </div>
-//             <div className="mt-4 md:mt-0 md:ml-4 w-full md:w-auto text-center">
-//               <button
-//                 className={`w-full md:w-auto px-4 py-2 rounded ${
-//                   v.isAccepted ? "bg-green-500" : "bg-red-500"
-//                 } text-white`}
-//                 onClick={() => toggleAcceptance(v.id)}
-//               >
-//                 {v.isAccepted ? "Accepted" : "Not Accepted"}
-//               </button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// const VoiceRecorder = () => {
-//   const { addVoice } = useStore();
-//   const [recording, setRecording] = useState<boolean>(false);
-//   const [quote, setQuote] = useState("");
-
-//   // Fetch random quote
-//   const fetchQuote = async () => {
-//     const res = await fetch("https://api.quotable.io/random");
-//     const data = await res.json();
-//     console.log(res);
-//     console.log(data);
-//     setQuote(data.content);
-//     alert(data.content);
-//   };
-
-//   console.log(quote);
-
-//   useEffect(() => {
-//     fetchQuote();
-//   }, []);
-
-//   const startRecording = () => setRecording(true);
-//   const stopRecording = () => setRecording(false);
-
-//   const onStop = (recordedBlob: { blob: Blob }) => {
-//     console.log("Recorded Blob:", recordedBlob);
-//     addVoice(recordedBlob.blob);
-//   };
-
-//   return (
-//     <div className="w-full max-w-xl mx-auto mt-20 p-4 bg-slate-100 border rounded-lg shadow-lg text-center">
-//       <h2 className="text-xl font-bold mb-4">Voice Recorder</h2>
-
-//       <VoiceCounter />
-
-//       <div className="mt-6">
-//         <div className="p-3 rounded-lg bg-gray-200">
-//           <ReactMic
-//             className="w-full"
-//             record={recording}
-//             onStop={onStop}
-//             mimeType="audio/wav"
-//             strokeColor="#FF0000"
-//             backgroundColor="#E0E0E0"
-//           />
-//         </div>
-
-//         <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
-//           <button
-//             className="w-full sm:w-auto px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition"
-//             // onClick={}
-//           >
-//             Take a quote
-//           </button>
-//           <button
-//             className="w-full sm:w-auto px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition"
-//             onClick={startRecording}
-//           >
-//             Start Recording
-//           </button>
-//           <button
-//             className="w-full sm:w-auto px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600 transition"
-//             onClick={stopRecording}
-//           >
-//             Stop Recording
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VoiceRecorder;
-
 "use client";
 
 import dynamic from "next/dynamic";
@@ -182,6 +19,8 @@ export interface VoiceStore {
   currentQuoteId: number;
   addVoice: (newVoice: Blob) => void;
   toggleAcceptance: (id: number) => void;
+  nextQuote: () => void;
+  prevQuote: () => void;
 }
 
 export interface Voice {
@@ -209,8 +48,24 @@ const useStore: UseBoundStore<StoreApi<VoiceStore>> = create<VoiceStore>(
         url: "/audio/quote-1.mp3",
         createdAt: new Date(),
       },
+      {
+        id: 2,
+        name: "Alice in Wonderland",
+        // url: "https://ia904602.us.archive.org/29/items/alicesadventure_abridged_pc_librivox/alicesadventuresinwonderlandabridged_01_carroll_64kb.mp3",
+        url: "/audio/quote-2.mp3",
+        createdAt: new Date(),
+      },
+      {
+        id: 3,
+        name: "Alice in Wonderland",
+        // url: "https://ia904602.us.archive.org/29/items/alicesadventure_abridged_pc_librivox/alicesadventuresinwonderlandabridged_01_carroll_64kb.mp3",
+        url: "/audio/quote-3.mp3",
+        createdAt: new Date(),
+      },
     ],
+
     currentQuoteId: 1,
+
     addVoice: (newVoice: Blob) =>
       set((state) => ({
         voices: [
@@ -218,11 +73,27 @@ const useStore: UseBoundStore<StoreApi<VoiceStore>> = create<VoiceStore>(
           voice.init(state.voices.length + 1, newVoice),
         ],
       })),
+
     toggleAcceptance: (id: number) =>
       set((state) => ({
         voices: state.voices.map((v) =>
           v.id === id ? { ...v, isAccepted: !v.isAccepted } : v
         ),
+      })),
+
+    nextQuote: () =>
+      set((state) => ({
+        currentQuoteId:
+          state.currentQuoteId < state.quotes.length
+            ? state.currentQuoteId + 1
+            : state.currentQuoteId,
+      })),
+    prevQuote: () =>
+      set((state) => ({
+        currentQuoteId:
+          state.currentQuoteId > 1
+            ? state.currentQuoteId - 1
+            : state.currentQuoteId,
       })),
   })
 );
@@ -251,16 +122,6 @@ function VoiceCounter() {
                   src={URL.createObjectURL(v.blob)}
                   className="w-full"
                 />
-
-                {/* <audio
-                  controls
-                  src={
-                    typeof window !== "undefined"
-                      ? URL.createObjectURL(v.blob)
-                      : ""
-                  }
-                  className="w-full"
-                /> */}
               </div>
               <div className="mt-4 md:mt-0 md:ml-4 w-full md:w-auto text-center">
                 <button
@@ -281,9 +142,9 @@ function VoiceCounter() {
 }
 
 const VoiceRecorder = () => {
-  const { addVoice, quotes, currentQuoteId } = useStore();
+  const { addVoice, quotes, currentQuoteId, nextQuote, prevQuote } = useStore();
   const [recording, setRecording] = useState<boolean>(false);
-  const [quote, setQuote] = useState<string | undefined>(undefined);
+  // const [quote, setQuote] = useState<string | undefined>(undefined);
 
   const chosenQuote = quotes.find((q) => q.id == currentQuoteId);
 
@@ -332,54 +193,97 @@ const VoiceRecorder = () => {
 
       <VoiceCounter />
 
-      <div className="mt-6">
-        <div className="p-3 rounded-lg bg-gray-200">
-          {/* {typeof window !== "undefined" && ( */}
-          <ReactMic
-            className="w-full"
-            record={recording}
-            onStop={onStop}
-            mimeType="audio/wav"
-            strokeColor="#FF0000"
-            backgroundColor="#E0E0E0"
-          />
-          {/* )} */}
-        </div>
+      <ReactMic
+        className="w-full m-2 p-2 rounded-lg bg-gray-200"
+        record={recording}
+        onStop={onStop}
+        mimeType="audio/wav"
+        strokeColor="#FF0000"
+        backgroundColor="#E0E0E0"
+      />
 
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4">
-          <blockquote>
-            Alice was beginning to get very tired of sitting by her sister on
-            the bank, <p></p>
-          </blockquote>
-          <blockquote>
-            and of having nothing to do: once or twice she had peeped into the
-            book her sister was reading,{" "}
-          </blockquote>
-          <blockquote>
-            but it had no pictures or conversations in it, “and what is the use
-            of a book,” thought Alice “without pictures or conversations?”
-          </blockquote>
-
-          {chosenQuote && (
+      <div className="flex-row sm:flex-row justify-center items-center gap-4 mt-4">
+        <blockquote
+          className={`${
+            chosenQuote?.id == 1 ? "border-2 border-gray-500 rounded-lg" : ""
+          } m-2 p-2 max-w-full sm:max-w-2xl lg:max-w-4xl`}
+        >
+          Alice was beginning to get very tired of sitting by her sister on the
+          bank,
+          {chosenQuote?.id == 1 && (
             <audio controls>
-              <source src={chosenQuote.url} type="audio/mpeg" />
+              <source src={chosenQuote.url} type="audio/mp3" />
               Your browser does not support the audio tag.
             </audio>
           )}
+        </blockquote>
+
+        <blockquote
+          className={`${
+            chosenQuote?.id == 2 ? "border-2 border-gray-500 rounded-lg" : ""
+          } m-2 p-2 max-w-full sm:max-w-2xl lg:max-w-4xl`}
+        >
+          and of having nothing to do: once or twice she had peeped into the
+          book her sister was reading,
+          {chosenQuote?.id == 2 && (
+            <audio controls>
+              <source src={chosenQuote.url} type="audio/mp3" />
+              Your browser does not support the audio tag.
+            </audio>
+          )}
+        </blockquote>
+
+        <blockquote
+          className={`${
+            chosenQuote?.id == 3 ? "border-2 border-gray-500 rounded-lg" : ""
+          } m-2 p-2 max-w-full sm:max-w-2xl lg:max-w-4xl`}
+        >
+          but it had no pictures or conversations in it, “and what is the use of
+          a book,” thought Alice “without pictures or conversations?”
+          {chosenQuote?.id == 3 && (
+            <audio controls>
+              <source src={chosenQuote.url} type="audio/mp3" />
+              Your browser does not support the audio tag.
+            </audio>
+          )}
+        </blockquote>
+        {/* {chosenQuote && (
+          <audio controls>
+            <source src={chosenQuote.url} type="audio/mp3" />
+            Your browser does not support the audio tag.
+          </audio>
+        )} */}
+
+        <div className="flex justify-center gap-4 mt-4">
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+            onClick={prevQuote}
+            disabled={currentQuoteId === 1}
+          >
+            Previous Quote
+          </button>
 
           <button
-            className="w-full sm:w-auto px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition"
-            onClick={startRecording}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+            onClick={nextQuote}
+            disabled={currentQuoteId === quotes.length}
           >
-            Start Recording
-          </button>
-          <button
-            className="w-full sm:w-auto px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600 transition"
-            onClick={stopRecording}
-          >
-            Stop Recording
+            Next Quote
           </button>
         </div>
+
+        <button
+          className="w-full sm:w-auto px-6 py-3 mx-6 bg-green-500 text-white rounded hover:bg-green-600 transition"
+          onClick={startRecording}
+        >
+          Start Recording
+        </button>
+        <button
+          className="w-full sm:w-auto px-6 py-3 bg-red-500 text-white rounded hover:bg-red-600 transition"
+          onClick={stopRecording}
+        >
+          Stop Recording
+        </button>
       </div>
     </div>
   );
